bcd4e50bea63cbeadb7e6fec1d5fd016e5eb8523
Ndukwe Onyinyechi <ljenclade@yahoo.com>
2024-02-25 11:22:22 +0000

    modifications

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = " https://jehn6190.github.io/aws-Terraform-nginx/";
 
 ghpages.publish(
   pathname,
